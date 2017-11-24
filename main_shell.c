b6f08d176c6d26f43432a0d565d51389167ba632
Mircea Dan <mircea_dan97@yahoo.com>
2017-11-24 19:44:57 +0200

    break the line into arguments, added support for external functions -execvp-

--- main_shell.c
+++ main_shell.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/wait.h>
 
 #include <readline/readline.h>
 #include <readline/history.h>
@@ -10,16 +11,35 @@ typedef int bool;
 #define true 1
 #define false 0
 
-bool interpretLine(char* buffer){
+//all the supported commands
+char* univ_commands[3] = {"exit", "-help", NULL};
 
-    if(strncmp(buffer, "exit", strlen(buffer)-1) == 0){
+void printHelp(){
 
+    printf("\n\nType:\n\n#exit to exit the terminal\n#ls (-l, -r) to get a list of all\n\n");
+
+}
+
+bool interpretLine(char* buffer, char** tokens){
+
+
+    if(strcmp(buffer, univ_commands[0]) == 0){
         return false;
+
+    }else if(strcmp(buffer, univ_commands[1]) == 0){
+
+        printHelp();
+
+    }else if(execvp(tokens[0], tokens) != -1){
+
+        return true;
 
     } else {
 
         printf("I don't know this command, see -help for info\n");
     }
+
+    //free(tokens);
 
     return true;
 
@@ -29,9 +49,8 @@ bool interpretLine(char* buffer){
 /*char* getLine(){
 
     char *buffer = NULL;
-    ssize_t buffSize = 0;
 
-    getline(&buffer, &buffSize, stdin);
+    buffer = readline(">>");
 
     return buffer;
 
@@ -39,26 +58,52 @@ bool interpretLine(char* buffer){
 
 }*/
 
+void parse(char* line, char** arg){
+
+    while(*line != '\0'){
+
+        while(*line == ' ' || *line == '\t' || *line == '\n'){
+
+            *line++ = '\0';
+        }
+
+        *arg++ = line;
+
+        while(*line != '\0' && *line != '\n' && *line != '\t' && *line != ' '){
+
+            line++;
+        }
+
+    }
+
+    *arg = '\0';
+
+}
+
 /*main loop*/
 void command_loop(){
 
     bool status = true;
     char* line;
+    char* arguments[80];
 
     while(status){
 
         /*implement arrow keys*/
 
-        line = readline(">_");
+        line = readline(">>");
+
+        parse(line, arguments);
 
         if(strlen(line) != 1){ //empty string
 
         add_history(line);
-        status = interpretLine(line);
+        status = interpretLine(line, arguments);
 
         }
 
     }
+
 
 
 
